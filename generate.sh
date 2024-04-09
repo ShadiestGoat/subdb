b760a8fb5833c30077483e8199adea0563ff5541
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-04-10 00:17:10 +0100

    Allow tpl generation debug

--- generate.sh
+++ generate.sh
@@ -1,1 +1,5 @@
-cat "tpl/$1.yaml" | tpl -f "tpl/$1.tpl" -d yaml -n "$1.tpl" | gofmt -s > "$1.go"
+IFS=""
+
+tpl_out=$(cat "tpl/$1.yaml" | tpl -f "tpl/$1.tpl" -d yaml -n "$1.tpl")
+
+echo $tpl_out | gofmt -s > "$1.go" || echo $tpl_out
