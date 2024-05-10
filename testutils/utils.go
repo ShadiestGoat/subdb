c44b72e15970faa12c735cddcbfd0659234494f5
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-05-10 20:47:43 +0100

    Make GenericQueryExpectation more robust

--- testutils/utils.go
+++ testutils/utils.go
@@ -95,7 +95,7 @@ func GenerateGenericQueryTest(
 	}
 }
 
-func GenericQueryExpectation(idp *subdb.IDPointer[int], opts *QuerySetupOpts) (eFirst, eLast int) {
+func GenericQueryExpectation(idp *subdb.IDPointer[int], opts *QuerySetupOpts) (eFirst, eLast, eLen int, eEarly bool) {
 	dir := 1
 
 	if opts.OldToNew != opts.NewestIsBiggest {
@@ -121,13 +121,35 @@ func GenericQueryExpectation(idp *subdb.IDPointer[int], opts *QuerySetupOpts) (e
 	d := opts.QuerySize - 1
 	eLast = eFirst + (d * dir)
 
+	if eFirst < 0 {
+		eLen = 0
+		eFirst = 0
+	}
+	if eLast < 0 {
+		eLast = 0
+	}
+	
+	if eFirst >= opts.DataSize {
+		eFirst = opts.DataSize - 1
+	}
+	if eLast >= opts.DataSize {
+		eLast = opts.DataSize - 1
+	}
+
+	if eFirst == eLast && idp != nil && idp.ExcludePointer {
+		eLen = 0
+	} else {
+		eLen = int(math.Abs(float64(eFirst) - float64(eLast))) + 1
+		eEarly = eLen == opts.QuerySize
+	}
+
 	return
 }
 
 func GenerateGenericTestName(n string, idp *subdb.IDPointer[int], opts *QuerySetupOpts) string {
 	name := []string{n}
 	if idp != nil {
-		name = append(name, "idp")
+		name = append(name, "idp", fmt.Sprint(idp.ID))
 		if idp.ExcludePointer {
 			name = append(name, "excl")
 		}
@@ -155,14 +177,20 @@ func GenericReadQueryTest(idp *subdb.IDPointer[int], opts *QuerySetupOpts, t *te
 			T:     t,
 		}
 
+		eFirst, eLast, eLen, eEarly := GenericQueryExpectation(idp, opts)
 		o, early := b.Read(idp, opts.OldToNew, f)
 
-		if !early || len(o) != opts.QuerySize {
-			t.Logf("Failed query, expected early exit with 10 values, got: early: %v, vals: %#v", early, o)
-			t.FailNow()
+		if len(o) != eLen {
+			t.Fatalf("Unexpected output len: %v, expected %v", len(o), eLen)
 		}
 
-		eFirst, eLast := GenericQueryExpectation(idp, opts)
+		if early != eEarly {
+			t.Fatalf("Failed query, unexpected early exit status: %v, expected %v", early, eEarly)
+		}
+
+		if eLen == 0 {
+			return
+		}
 
 		rFirst, rLast := o[0].GetID(), o[len(o)-1].GetID()
 
@@ -175,7 +203,12 @@ func GenericReadQueryTest(idp *subdb.IDPointer[int], opts *QuerySetupOpts, t *te
 	})
 }
 
-func GenericDeleteQueryTest(idp *subdb.IDPointer[int], opts *QuerySetupOpts, t *testing.T, b subdb.BackendWithEverything[int]) {
+	eFirst, eLast, eLen, _ := GenericQueryExpectation(idp, opts)
+
+	if eLen == 0 {
+		return
+	}
+
 	t.Run(GenerateGenericTestName("delete", idp, opts), func(t *testing.T) {
 		f := &Filter[int]{
 			Limit: opts.QuerySize,
@@ -183,8 +216,6 @@ func GenericDeleteQueryTest(idp *subdb.IDPointer[int], opts *QuerySetupOpts, t *
 		}
 
 		b.Delete(idp, opts.OldToNew, f)
-
-		eFirst, eLast := GenericQueryExpectation(idp, opts)
 
 		o := b.ReadID(eFirst, eLast)
 
